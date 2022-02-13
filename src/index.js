2817806ed2110eec058094cd0921a16fe3004053
fortune7s <yuliya19891986@rambler.ru>
2022-02-13 16:18:36 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(num) {
+    let result = 0;
+    if(num>0){
+      while (num) {
+        result = result * 10 + num % 10;
+        console.log(result);
+        num = Math.floor(num / 10);
+        console.log(num);
+      }
+    
+      return result;
+    } else{
+      num = num*-1;
+    }
+    while (num) {
+      result = result * 10 + num % 10;
+      console.log(result);
+      num = Math.floor(num / 10);
+      console.log(num);
+    }
+    return result;
 }
+
